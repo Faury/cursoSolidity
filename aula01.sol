b8e840891289424df33e8dbe37bf9e4404f6cea0
Faury <faury.lima@gmail.com>
2023-06-12 17:53:53 -0300

    Create aula01.sol

---
+++ aula01.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+contract ExampleContract {
+    function helloWorld() public pure returns (uint256) {
+        return 100;
+    }
+
+    function haloDunial() public pure returns (bool) {
+        return true;
+    }
+}
